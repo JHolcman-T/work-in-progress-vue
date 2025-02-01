58d10da12b8edba59a04bf01d120e7013e8d3e37
Jakub Holcman <kontakt@jakub-holcman.com>
2025-02-01 12:08:31 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # work-in-progress-vue
+
 Work-in-Progress Vue is a simple static website built with Vue, designed to display a 'Work in Progress' banner on your server while the main site is under development.
+
+## Icons
+
+https://icones.js.org/collection/openmoji?s=construction
---
+++ src/components/Main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import OpenmojiConstruction from "./icons/OpenmojiConstruction.vue"
+  import OpenmojiBuildingConstruction from "./icons/OpenmojiBuildingConstruction.vue"
+</script>
+
+<template>
+  <h1>Work in progress...</h1>
+  <OpenmojiConstruction width="5em" height="5em"></OpenmojiConstruction>
+  <OpenmojiBuildingConstruction width="10em" height="10em"></OpenmojiBuildingConstruction>
+  <OpenmojiConstruction width="5em" height="5em"></OpenmojiConstruction>
+</template>
